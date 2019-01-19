f3e1c4b06d3f9d4eb8d585068824f96992f986dc
pvv <pvvchip@mail.ru>
2019-01-19 07:53:04 +0300

    Start

--- client/src/main/java/Controller.java
+++ client/src/main/java/Controller.java
@@ -1,2 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField msgField;
+
+    public void sendMsg(ActionEvent actionEvent) {
+        textArea.appendText(msgField.getText() + '\n');
+        msgField.clear();
+        msgField.requestFocus();
+    }
 }
