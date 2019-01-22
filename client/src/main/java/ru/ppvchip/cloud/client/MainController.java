53f95c63ad192066c3c44eb303c4ea879a5e7810
pvv <pvvchip@mail.ru>
2019-01-23 01:11:55 +0300

    lesson_3 - start

---
+++ client/src/main/java/ru/ppvchip/cloud/client/MainController.java
@@ -0,0 +1,80 @@
+package ru.ppvchip.cloud.client;
+
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import ru.ppvchip.cloud.common.AbstractMessage;
+import ru.ppvchip.cloud.common.FileMessage;
+import ru.ppvchip.cloud.common.FileRequest;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    TextField tfFileName;
+
+    @FXML
+    ListView<String> filesList;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Network.start();
+        Thread t = new Thread(() -> {
+            try {
+                while (true) {
+                    AbstractMessage am = Network.readObject();
+                    if (am instanceof FileMessage) {
+                        FileMessage fm = (FileMessage) am;
+                        Files.write(Paths.get("storage_client/" + fm.getFilename()), fm.getData(), StandardOpenOption.CREATE);
+                        refreshLocalFilesList();
+                    }
+                }
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            } finally {
+                Network.stop();
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        filesList.setItems(FXCollections.observableArrayList());
+        refreshLocalFilesList();
+    }
+
+    public void pressOnDownloadBtn(ActionEvent actionEvent) {
+        if (tfFileName.getLength() > 0) {
+            Network.sendMsg(new FileRequest(tfFileName.getText()));
+            tfFileName.clear();
+        }
+    }
+
+    public void refreshLocalFilesList() {
+        if (Platform.isFxApplicationThread()) {
+            try {
+                filesList.getItems().clear();
+                Files.list(Paths.get("storage_client")).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Platform.runLater(() -> {
+                try {
+                    filesList.getItems().clear();
+                    Files.list(Paths.get("storage_client")).map(p -> p.getFileName().toString()).forEach(o -> filesList.getItems().add(o));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+}
